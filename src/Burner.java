2edcc81eb1813b9f6e6cf84f7deb460983c02ff4
Chris Blaylock <cblaylock@mymail.mines.com>
2021-02-01 21:01:23 -0700

    Updated Burner, Stove, Setting

---
+++ src/Burner.java
@@ -0,0 +1,19 @@
+
+public class Burner {
+
+	public void plusButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void minusButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateTemperature() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
